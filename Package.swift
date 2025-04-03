23f766518cba9c171c0acbda0f7dfa94b1e0b5b4
lalit <lalit.vinde@cometchat.com>
2025-04-03 17:26:23 +0530

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "CometChatSealdSDK",
+    platforms: [
+        .iOS(.v14) // Specify iOS 13+ support
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CometChatSealdSDK",
             targets: ["CometChatSealdSDK"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/seald/seald-sdk-ios", from: "0.9.0"),
+        .package(url: "https://github.com/cometchat/chat-sdk-ios", from: "4.0.55")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CometChatSealdSDK"),
+            name: "CometChatSealdSDK",
+            dependencies: [
+                .productItem(name: "SealdSdk", package: "seald-sdk-ios", moduleAliases: nil, condition: nil),
+                .productItem(name: "CometChatSDK", package: "chat-sdk-ios", moduleAliases: nil, condition: nil)
+            ]
+        ),
 
     ]
 )
